ac7d533b8fb7aef4f2257c97578f95637ca808d0
pandong <pandong@adss-laser.com.cn>
2018-11-07 18:36:35 +0800

    C# SackHeap

---
+++ SackHeapWork/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SackHeapWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = ReturnValue2();
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+        public static int ReturnValue()
+        {
+            int x = new int();
+            x = 3;
+            int y = new int();
+            y = x;
+            y = 4;
+            return x;
+        }
+        public static int ReturnValue2()
+        {
+            MyInt x = new MyInt();
+           
+            x.MyValue = 3;
+            MyInt y = new MyInt();
+            y = x;
+            y.MyValue = 4;
+            return x.MyValue;
+        }
+
+    }
+
+    public class MyInt
+    {
+        public int MyValue;
+    }
+}
